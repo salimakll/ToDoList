6fbef6d70a9b9133fc8b1dd7b2b6410814218a52
Salim Akıllı <126464803+salimakll@users.noreply.github.com>
2023-05-18 16:08:03 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let addToButton = document.getElementById('addToDo');
+let toDoContainer = document.getElementById('toDoContainer');
+let inputText = document.getElementById('inputText');
+let clearToDo = document.getElementById('clearToDo');
+
+addToButton.addEventListener('click',function(){
+    let paragraph = document.createElement('p');
+    paragraph.classList.add('paragraph-styling');
+    toDoContainer.appendChild(paragraph);
+    paragraph.innerHTML = inputText.value;
+    inputText.value= '';
+
+    paragraph.addEventListener('click' ,function(){
+        paragraph.style.textDecoration='line-through';
+
+    })
+    
+    paragraph.addEventListener('dblclick', function(){
+        toDoContainer.removeChild(paragraph);
+
+    })
+    
+    clearToDo.addEventListener('click', function(){
+       // toDoContainer.removeChild(paragraph);
+       paragraph.style.display='none';
+      // paragraph.remove();
+    })
+})
+
